3796d99f4799ab4069ee3c419e79136d4deeec88
Soton <77579218+soton371@users.noreply.github.com>
2021-07-10 20:20:35 +0600

    Add files via upload

--- identify odd or even.asm
+++ identify odd or even.asm
@@ -1,48 +1,33 @@
-.MODEL SMALL
- .STACK 100H
-
- .DATA
-    PROMPT_1  DB  'Enter the number 0 to 9 : $'
-    PROMPT_2  DB  0DH,0AH,'The number is : $' 
-    PROMPT_3  DB  'EVEN'
-    PROMPT_4  DB  'ODD'
-
- .CODE
-   MAIN PROC
-     MOV AX, @DATA                
-     MOV DS, AX                   
-
-     LEA DX, PROMPT_1             
-     MOV AH, 9
-     INT 21H
-
-     MOV AH, 1                    
-     INT 21H
-
-     MOV BL, AL                    
-     
-     LEA DX, PROMPT_2             
-     MOV AH, 9
-     INT 21H
-
-     TEST BL, 01H                 
-
-     JNE @ODD                     
-
-     MOV AH, 2                    
-     MOV DL, PROMPT_3
-     INT 21H
-
-     JMP @EXIT                    
-
-     @ODD:                        
-       MOV AH, 2                  
-       MOV DL, PROMPT_4
-       INT 21H
-
-     @EXIT:                       
-
-     MOV AH, 4CH                  
-     INT 21H
-   MAIN ENDP 
- END MAIN
+DATA SEGMENT
+MSG1 DB 10,13,'Enter the number from 0 to 9: $'
+MSG2 DB 10,13,'The number is EVEN$'
+MSG3 DB 10,13,'The number is ODD$'
+DATA ENDS
+DISPLAY MACRO MSG
+MOV AH,9
+LEA DX,MSG
+INT 21H
+ENDM
+CODE SEGMENT
+ASSUME CS:CODE,DS:DATA
+START:
+MOV AX,DATA
+MOV DS,AX
+DISPLAY MSG1
+MOV AH,1
+INT 21H
+MOV AH,0
+CHECK: MOV DL,2
+DIV DL
+CMP AH,0
+JNE ODD
+EVEN:
+DISPLAY MSG2
+JMP DONE
+ODD:
+DISPLAY MSG3
+DONE:
+MOV AH,4CH
+INT 21H
+CODE ENDS
+END START
